812cd4a1ba2e7f1e5644c57a6a237259c7acfc61
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-09-09 22:22:26 +0300

    upate

---
+++ Functions.js
@@ -0,0 +1,37 @@
+// function sayHi(){
+//     console.log("hell")
+//     console.log("Bye")
+// }
+// sayHi()
+
+
+
+// function sum (a, b){
+//     console.log(b)
+//     console.log(a + b)
+// }
+
+// sum (1, 2)
+// const a = 1 
+// const b = 2
+// console.log(b)
+// console.log(a + b)
+
+
+// function sum (a, b){
+// console.log(a)
+// console.log(b)
+// }
+
+// sum (1)
+// // b undefined
+
+
+
+
+function sum (a, b){
+    return a + b
+}
+
+const value = sum (1, 2)
+console.log(value)
